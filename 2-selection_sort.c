8a7ae7164af96084ba5929d66586cc8b427e6b6b
Ompheletse Makwela <omphentse@gmail.com>
2024-01-25 04:38:31 +0200

    task2

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * exc_ints - exchanges two integers in an array.
+ * @x: The first integer to exchange.
+ * @y: The second integer to exchange.
+ */
+void exc_ints(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * selection_sort - Sorts an array of integers in ascending order.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *minimum;
+	size_t i, n;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (n = i + 1; n < size; j++)
+			miniimum = (array[n] < *minimum) ? (array + n) : minimum;
+
+		if ((array + i) != minimum)
+		{
+			exc_ints(array + i, minimum);
+			print_array(array, size);
+		}
+	}
+}
