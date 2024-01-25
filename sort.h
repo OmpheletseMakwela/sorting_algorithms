1120ba08dfb10c0b4b1afb4b2cbfc68689fd3b68
Ompheletse Makwela <omphentse@gmail.com>
2024-01-25 04:31:28 +0200

    header file

--- sort.h
+++ sort.h
@@ -1,55 +1,17 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+
 #define UP 0
 #define DOWN 1
 
 /**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
-}
-
-/**
- * print_list - Prints a list of integers
- *
- * @list: The list to be printed
- */
-void print_list(const listint_t *list)
-{
-    int i;
-
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
-}
-
-/**
- * enum bool - Enumeration of the boolean values.
- * @false: Equals to 0.
- * @true: Equals to 1.
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
  */
 typedef enum bool
 {
@@ -57,11 +19,33 @@ typedef enum bool
 	true
 } bool;
 
+/**
+ * struct listint_s - Doubly linked list node
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void esc_ints(int *x, int *y);
-void bubble_sort(int *array, size_t size);
-void esc_nodes(listint_t **h, listint_t **node1, listint_t *node2);
-void insertion_sort_list(listint_t **list);
 
-#endif /*SORT_H*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
